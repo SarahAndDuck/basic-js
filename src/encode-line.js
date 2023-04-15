39a314730593e14dc5e17e8c977b72e55aea29e5
SarahAndDuck <ratkevirina@gmail.com>
2023-04-16 01:02:59 +0700

    feat: add implement encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // throw new NotImplementedError("Not implemented");
+  // // remove line with error and write your code here
+  let obj = {};
+  let newArr = str.split("");
+  let newArr2 = [];
+  console.log(newArr);
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] == newArr[i + 1]) {
+      if (obj[newArr[i]]) obj[newArr[i]]++;
+      else obj[newArr[i]] = 1;
+    } else {
+      if (obj[newArr[i]]) obj[newArr[i]]++;
+      else obj[newArr[i]] = 1;
+      newArr2.push(...Object.entries(obj));
+      obj = {};
+    }
+  }
+  console.log(newArr2);
+  let newArr3 = [];
+  newArr2.forEach((item) => {
+    if (item[1] > 1) newArr3.push(item[1] + item[0]);
+    else newArr3.push(item[0]);
+  });
+  return newArr3.join("");
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
