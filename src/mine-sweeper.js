cb2564ef08a023064bd7fd38db13030d4ae284af
SarahAndDuck <ratkevirina@gmail.com>
2023-04-16 14:17:03 +0700

    feat: add solution for sort-by-height

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
+  let matrixResult = [];
+  for (let i = 0; i < matrix.length; i++) {
+    matrixResult.push([]);
+    for (let j = 0; j < matrix[0].length; j++) matrixResult[i].push(0);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] == true && i > 0 && j > 0) {
+        matrixResult[i - 1][j - 1] += 1;
+        matrixResult[i - 1][j] += 1;
+        matrixResult[i - 1][j + 1] += 1;
+        matrixResult[i][j - 1] += 1;
+        matrixResult[i][j + 1] += 1;
+        matrixResult[i + 1][j - 1] += 1;
+        matrixResult[i + 1][j] += 1;
+        matrixResult[i + 1][j + 1] += 1;
+      } else if (matrix[i][j] == true && i == 0 && j == 0) {
+        // matrixResult[i ][j ] += 1;
+        matrixResult[i][j + 1] += 1;
+        matrixResult[i + 1][j + 1] += 1;
+        matrixResult[i + 1][j] += 1;
+      } else if (
+        matrix[i][j] == true &&
+        i == matrix.length - 1 &&
+        j == matrix[0].length - 1
+      ) {
+        // matrixResult[i ][j ] += 1;
+        matrixResult[i][j - 1] += 1;
+        matrixResult[i - 1][j - 1] += 1;
+        matrixResult[i - 1][j] += 1;
+      } else if (matrix[i][j] == true && i == 0 && j == matrix[0].length - 1) {
+        // matrixResult[i ][j ] += 1;
+        matrixResult[i][j - 1] += 1;
+        matrixResult[i + 1][j - 1] += 1;
+        matrixResult[i + 1][j] += 1;
+      }
+    }
+  }
+  return matrixResult;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
+  let newArr1 = [];
+  let newArr2 = [];
+  let newArr3 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == -1) newArr1.push(i);
+    else newArr2.push(arr[i]);
+  }
+  //   console.log(newArr1);
+  let newArr2Sort = newArr2.sort((a, b) => (a > b ? 1 : -1));
+  let newArr1Set = new Set(newArr1);
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (newArr1Set.has(i)) newArr3.push(-1);
+    else {
+      newArr3.push(newArr2Sort[j]);
+      j++;
+    }
+  }
+
+  return newArr3;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
