f6f1eb8bdd04086a26f489a92a1c12642e213265
SarahAndDuck <ratkevirina@gmail.com>
2023-04-16 16:14:48 +0700

    feat: add solution for extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
+  let { ...rest } = options;
+
+  if (rest.additionSeparator == undefined) rest.additionSeparator = "|";
+  if (rest.separator == undefined) rest.separator = "+";
+  if (rest.repeatTimes == undefined) rest.repeatTimes = 1;
+
+  console.log(rest);
+  let newStr = "";
+  for (let i = 0; i < rest.repeatTimes; i++) {
+    newStr += String(str);
+    let counRepeatAddition = 1;
+    if (rest.additionRepeatTimes !== undefined)
+      counRepeatAddition = rest.additionRepeatTimes;
+    for (let i = 0; i < counRepeatAddition; i++) {
+      if (rest.addition !== undefined) newStr += String(rest.addition);
+      if (
+        rest.additionSeparator &&
+        counRepeatAddition > 1 &&
+        i < counRepeatAddition - 1
+      )
+        newStr += String(rest.additionSeparator);
+    }
+    if (rest.separator !== undefined && i !== rest.repeatTimes - 1)
+      newStr += String(rest.separator);
+  }
+  return newStr;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
